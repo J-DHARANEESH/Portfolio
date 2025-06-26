18e9d20092980a108ef1df4803008747a2336430
Dharaneesh <j.dharaneesh12@gmail.com>
2025-06-26 12:08:26 +0530

    variable name fixed for email

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
-  readonly NG_APP_EMAILJS_SERVICE_ID: string;
-  readonly NG_APP_EMAILJS_TEMPLATE_ID: string;
-  readonly NG_APP_EMAILJS_PUBLIC_KEY: string;
+  readonly EMAILJS_SERVICE_ID: string;
+  readonly EMAILJS_TEMPLATE_ID: string;
+  readonly EMAILJS_PUBLIC_KEY: string;
 }
 
 interface ImportMeta {
